8f66a95f30a5ced504c9580a6a9a0783cdffc763
jojifenix <jordi.eduardo.flores.flores.1996@gmail.com>
2024-12-18 18:13:26 +0100

    actualización 18/12/2024

--- 2_evaluacion/biblioteca(v3)/views/libro/all.php
+++ 2_evaluacion/biblioteca(v3)/views/libro/all.php
@@ -1,27 +1,20 @@
-<!--
-extraer todo lo de vista. Vamos a hacer un modelo por cada entidad y una vista por cada caso de uso
-¿y cada vista en una clase diferente?
-Aqui no hacemos el fetch, sino que recibimos el array ya hecho ($libros)
--->
-
 <h1>Listado de libros</h1>
 <table border='1'>
 
-    <?php
-   
-    // Cada libro ha de aparecer una sola vez
+<?php
+echo "<h1>Biblioteca</h1>";
 
-    echo "<h1>Biblioteca</h1>";
-
-    echo "<form action='" . $_SERVER['PHP_SELF'] . "'>
+echo "<form action='" . $_SERVER['PHP_SELF'] . "'>
     <input type='hidden' name='action' value='libroGet'>
-    <input type='text' name='textoBusqueda'> 
+    <input type='text' name='textoBusqueda'>
     <input type='submit' value='Buscar'>
-    </form>";
+    </form><br><br>";
 
+$actual = null; // Inicializamos con un valor que no coincida con ningún idLibro
+$autores = ""; // Inicializamos una cadena vacía
 
-    $actual =$data['libro_all'][0]; // Inicializamos con un valor que no coincida con ningún idLibro
-    $autores = ""; // Inicializamos una cadena vacía
+if (!empty($data['libro_all'])) {
+    $actual = $data['libro_all'][0];
     $campos = array_keys(get_object_vars($actual));
     unset($campos['nombre']);
     unset($campos['apellido']);
@@ -29,56 +22,45 @@ Aqui no hacemos el fetch, sino que recibimos el array ya hecho ($libros)
     foreach ($data['libro_all'] as $libro) {
         if ($libro->idLibro != $actual->idLibro) {
             echo "<tr>";
-            foreach($campos as $c) 
-                echo "<td>".$actual->$c."</td>";
-                echo"<td>".$autores."</td>";//autores acumulados
+            foreach ($campos as $c) {
+                echo "<td>" . $actual->$c . "</td>";
+            }
+            echo "<td>" . $autores . "</td>"; // autores acumulados
 
-                if (isset($_SESSION['iduser'])) {
-                    echo "</td><a href='".$_SERVER['PHP_SELF']."?action=libroPrestart&idLibro=".$actual."'>Prestar</a></td></tr>";
-                }
-        
-                if (isset($_SESSION['adm'])) {
-                    echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroForm&idLibro=" . $actual . "'>Modificar</a></td>
-                    <td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroDelete&idLibro=" . $actual . "'>Borrar</a></td>
-                    </tr>";
-                
-                }
+            if (isset($_SESSION['iduser'])) {
+                echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroPrestart&idLibro=" . $actual->idLibro . "'>Prestar</a></td>";
+            }
+
+            if (isset($_SESSION['adm'])) {
+                echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroForm&idLibro=" . $actual->idLibro . "'>Modificar</a></td>
+                      <td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroDelete&idLibro=" . $actual->idLibro . "'>Borrar</a></td>";
+            }
+            echo "</tr>";
+
+            $actual = $libro;
+            $autores = "";
         }
-    $actual = $libro->idLibro;
-    $autores = ""; 
+        $autores .= $libro->apellido . ", " . $libro->nombre . "<br>";
     }
 
-    // Acumulamos los autores
-    $autores .= $libro->apellido . ", " . $libro->nombre . "<br>";
-    
-
     // Imprimir el último libro
-        echo "<tr>";
-        foreach($campos as $c) echo "<td>".$actual->$c."</td>";
-        echo"<td>".$autores."</td>";//autores acumulados
+    echo "<tr>";
+    foreach ($campos as $c) {
+        echo "<td>" . $actual->$c . "</td>";
+    }
+    echo "<td>" . $autores . "</td>"; // autores acumulados
 
-        
+    if (isset($_SESSION['iduser'])) {
+        echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroPrestart&idLibro=" . $actual->idLibro . "'>Prestar</a></td>";
+    }
 
-        if (isset($_SESSION['iduser'])) {
-            echo "</td><a href='".$_SERVER['PHP_SELF']."?action=libroPrestart&idLibro=".$actual."'>Prestar</a></td></tr>";
-        }
-
-        if (isset($_SESSION['adm'])) {
-            echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroForm&idLibro=" . $actual . "'>Modificar</a></td>
-            <td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroDelete&idLibro=" . $actual . "'>Borrar</a></td>
-            </tr>";
-        
-        }
-    
-
-
-
-    ?>
-
-
-</table>
-<?php
-if (isset($rol) && $rol == "admin") {
-    echo "<p><a href='" . $_SERVER['PHP_SELF'] . "?action=libroForm'>Nuevo</a></p>";
+    if (isset($_SESSION['adm'])) {
+        echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroForm&idLibro=" . $actual->idLibro . "'>Modificar</a></td>
+              <td><a href='" . $_SERVER['PHP_SELF'] . "?action=libroDelete&idLibro=" . $actual->idLibro . "'>Borrar</a></td>";
+    }
+    echo "</tr>";
 }
 ?>
+
+</table>
+<p><a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>?action=libroForm">Nuevo</a></p>
